ab394580bfc8c54d2a8a4601f16549b5379c2439
Zorigto Badmain <badmainzzz@mail.ru>
2021-02-24 10:42:02 +0900

    Всплывающее окно Добавление дохода. Подключил и применил RxSwift

---
+++ FinanceDiplom/Income/AddIncomeViewController.swift
@@ -0,0 +1,78 @@
+//
+//  AddIncomeViewController.swift
+//  FinanceDiplom
+//
+//  Created by Зоригто Бадмаин on 23.02.2021.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class AddIncomeViewController: UIViewController {
+    
+    @IBOutlet weak var summatextField: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var closeButton: UIButton!
+    
+    private let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        buttonActivation()
+        closeButtonAction()
+    }
+    
+    func configure() {
+        summatextField.placeholder = "Сумма"
+        summatextField.keyboardType = .numberPad
+        summatextField.becomeFirstResponder()
+        addButton.layer.cornerRadius = 24
+    }
+    
+    func closeButtonAction() {
+        closeButton.rx.tap.subscribe(onNext: { [weak self] in
+            self?.summatextField.text = ""
+            self?.closeButton.isHidden = true
+            self?.addButton.isEnabled = false
+            self?.addButton.alpha = 0.1
+        }).disposed(by: disposeBag)
+    }
+    
+    func buttonActivation() {
+        let incomeValid: Observable<Bool> = summatextField.rx.text.map { (summa) -> Bool in
+            summa!.count > 0
+        }
+        
+        let incomeInt: Observable<Bool> = summatextField.rx.text.map { (income) -> Bool in
+            self.validateIncome(candidate: income!)
+        }
+        
+        Observable.combineLatest(incomeValid, incomeInt)
+            .subscribe(onNext: { summa, number in
+                if (summa, number) == (true, true) {
+                    self.closeButton.isHidden = false
+                    self.addButton.isEnabled = true
+                    self.addButton.alpha = 1
+                } else {
+                    self.closeButton.isHidden = true
+                    self.addButton.isEnabled = false
+                    self.addButton.alpha = 0.1
+                }
+                
+            }).disposed(by: disposeBag)
+    }
+    
+    func validateIncome(candidate: String) -> Bool {
+        let PHONE_REGEX = "\\d{1,10}"
+        let numberTest = NSPredicate(format: "SELF MATCHES %@", PHONE_REGEX)
+        return numberTest.evaluate(with: candidate)
+    }
+    
+    @IBAction func closeAction(_ sender: Any) {
+    }
+    
+    @IBAction func addIncomeAction(_ sender: Any) {
+    }
+}
--- FinanceDiplom/Income/IncomeViewController.swift
+++ FinanceDiplom/Income/IncomeViewController.swift
@@ -12,6 +12,7 @@ class IncomeViewController: UIViewController {
     @IBOutlet weak var myMoney: UILabel!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var addIncomeButton: UIButton!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     
     var myIncome: [Int] = []
     
@@ -19,37 +20,18 @@ class IncomeViewController: UIViewController {
         super.viewDidLoad()
         self.configureTable()
         self.configure()
+        
     }
     
     func configure() {
         addIncomeButton.layer.cornerRadius = 24
+        
         myMoney.text = "\(myIncome.first ?? 0) руб"
     }
     
     func configureTable() {
         tableView.delegate = self
         tableView.dataSource = self
-    }
-    
-    @IBAction func addIncomeAction(_ sender: Any) {
-        let ac = UIAlertController(title: "Сумма", message: .none, preferredStyle: .alert)
-        ac.addTextField { (textField) in
-            textField.placeholder = "Сумма"
-            textField.keyboardType = .numberPad
-        }
-        let alertAction = UIAlertAction(title: "Добавить доход", style: .cancel) { (alert) in
-            guard let newItem = ac.textFields?.last?.text else { return }
-            if newItem != "" {
-                let item = Int(newItem)
-                self.myIncome.append(item ?? 0)
-                let summ = self.myIncome.reduce( 0, +)
-                self.myMoney.text = "\(summ) руб"
-                self.tableView.reloadData()
-            }
-        }
-        
-        ac.addAction(alertAction)
-        present(ac, animated: true, completion: nil)
     }
     
 }
